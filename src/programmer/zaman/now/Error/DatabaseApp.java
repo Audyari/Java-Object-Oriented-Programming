82d1e32bc69ca45c65a982b988fd49ecde75decf
Audyari Wiyono <audy123ari@gmail.com>
2024-06-30 20:26:14 +0700

    Error di Java

---
+++ src/programmer/zaman/now/Error/DatabaseApp.java
@@ -0,0 +1,14 @@
+package programmer.zaman.now.Error;
+
+public class DatabaseApp{
+    public static void main(String[] args) {
+        connectDatabase("eko", "null");
+        System.out.println("Sukses");
+    }
+
+    public static void connectDatabase(String username, String password) {
+        if (username == null || password == null) {
+            throw new DatabaseError("Tidak bisa konek ke database");
+        }
+    }
+}
---
+++ src/programmer/zaman/now/Error/DatabaseError.java
@@ -0,0 +1,8 @@
+package programmer.zaman.now.Error;
+
+public class DatabaseError extends Error {
+
+    public DatabaseError(String message) {
+        super(message);
+    }
+}
